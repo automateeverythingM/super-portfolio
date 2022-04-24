b9976ad0bcc8124759b7006a3b219b9c44444394
automateeverythingM <markospavicbg@gmail.com>
2022-04-24 23:34:05 +0200

    theme toggler

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -4,26 +4,39 @@ import { Logo } from "../logo/logo";
 interface INavbarProps { }
 
 export const Navbar = ({ }: INavbarProps) => {
+
+    const toggleTheme = () => {
+        document.body.classList.toggle("dark");
+    };
+
     return (
-        <nav className="container mx-auto flex justify-between items-center py-5">
-            <Logo />
-            <ul className="flex">
-                <li>
-                    <Link href="/projects">
-                        <a className="nav-link">Projects</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/components">
-                        <a className="nav-link">Components</a>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about">
-                        <a className="nav-link">About</a>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+        <div className="flex items-center container mx-auto">
+            <nav className="flex w-full justify-between items-center py-5">
+                <Logo />
+                <ul className="flex">
+                    <li>
+                        <Link href="/projects">
+                            <a className="nav-link">Projects</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/components">
+                            <a className="nav-link">Components</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a className="nav-link">About</a>
+                        </Link>
+                    </li>
+                </ul>
+
+            </nav>
+            <div>
+                <button onClick={toggleTheme} className="whitespace-nowrap">
+                    Toggle Theme
+                </button>
+            </div>
+        </div>
     );
 };
