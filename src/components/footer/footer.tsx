39c9f014c4ba2eefcfb1d7f8ab6bd74d68b57495
automateeverythingM <markospavicbg@gmail.com>
2022-04-24 22:24:07 +0200

    basic footer for layout

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,12 @@
-interface IFooterProps {}
+interface IFooterProps { }
 
-export const Footer = ({}: IFooterProps) => {
-  return <div>Footer</div>;
+export const Footer = ({ }: IFooterProps) => {
+    return (
+        <footer className="bg-black text-white text-center py-3">
+            Visit my github for more information{" "}
+            <a className="text-yellow-200 hover:text-yellow-300" href="https://github.com/automateeverythingM" rel="noopener noreferrer" target="_blank">
+                @automateeverythingM
+            </a>
+        </footer>
+    );
 };
